d357c385813cb857f3c479c5e8bda4310068c993
Jaxwood <lorenzen.jacob@gmail.com>
2014-02-15 21:15:42 +0100

    added methods for getting digits of a number and to check if numbers is a palindrome

---
+++ Euler.Lib/PalindromeFinder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Euler.Lib
+{
+    public class PalindromeFinder
+    {
+        public int[] GetDigits(int number)
+        {
+            List<int> arr = new List<int>();
+
+            while(number >= 1)
+            {
+                arr.Add(number % 10);
+                number = number / 10;
+            }
+            
+            arr.Reverse();
+            
+            return arr.ToArray();
+        }
+
+        public bool IsPalindrome(int number)
+        {
+            bool isPalindrome = true;            
+            var digits = this.GetDigits(number);
+
+            for (int i = 0; i < digits.Length; i++)
+            { 
+                if (digits[i] != digits[(digits.Length - 1) - i])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            return isPalindrome;
+        }
+    }
+}
---
+++ Euler.Tests/PalindromeFinderTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+using Euler.Lib;
+
+namespace Euler.Tests
+{
+    public class PalindromeFinderTests
+    {
+        [Theory]
+        [InlineData(121, new int[] { 1, 2, 1 })]
+        [InlineData(321, new int[] { 3, 2, 1 })]
+        [InlineData(32211, new int[] { 3, 2, 2, 1, 1 })]
+        public void CanFindDigitsOfANumber(int number, int [] expected)
+        {
+            var sut = new PalindromeFinder();
+            var actual = sut.GetDigits(number);
+            
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(121, true)]
+        [InlineData(321, false)]
+        [InlineData(32211, false)]
+        [InlineData(9009, true)]
+        public void CanVerifyIfNumberIsAPalindrome(int number, bool expected)
+        {
+            var sut = new PalindromeFinder();
+            var actual = sut.IsPalindrome(number);
+            
+            Assert.Equal(expected, actual);
+        }
+    }
+}
