7652424e43417daf3864526c5c67f8c3251a7a3b
Jaxwood <lorenzen.jacob@gmail.com>
2014-02-17 21:49:06 +0100

    solution to problem 7

---
+++ Euler.Lib/LargestProductFinder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Euler.Lib
+{
+    public class LargestProductFinder
+    {
+        private int _range;
+        public LargestProductFinder(int range)
+        {
+            this._range = range;
+        }
+
+        public int GetLargestProduct(string series)
+        {
+            var myseries = this.ConvertToArray(series);
+            int found = 0;
+            for (int i = 0; i < myseries.Length; i++)
+            {
+                var product = myseries.Skip(i)
+                                    .Take(this._range)
+                                    .Aggregate(1, (a,c) => a *= c );
+
+                if ( product > found)
+                {
+                    found = product;
+                }                
+            }
+            return found;
+        }
+
+        private int[] ConvertToArray(string series)
+        {
+            return series.Select(c => (int)Char.GetNumericValue(c)).ToArray();
+        }
+    }
+}
---
+++ Euler.Tests/LargestProductFinderTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Euler.Lib;
+using Xunit.Extensions;
+
+namespace Euler.Tests
+{
+    public class LargestProductFinderTests
+    {
+        [Theory]
+        [InlineData(36, "236654", 2)]
+        public void ShouldFindTheLargestProductInASeries(int expected, string series, int range)
+        {
+            var sut = new LargestProductFinder(range);
+
+            var actual = sut.GetLargestProduct(series);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
