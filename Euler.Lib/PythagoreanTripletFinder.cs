edf53336a6ab49abd73d86109cb53c477be24e0e
Jaxwood <lorenzen.jacob@gmail.com>
2014-02-18 08:53:53 +0100

    solution to problem 9

---
+++ Euler.Lib/PythagoreanTripletFinder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Euler.Lib
+{
+    public class PythagoreanTripletFinder
+    {
+        public int[] GetTriplets(int sum)
+        {
+            for (int a = 2; a < sum; a++)
+            {
+                for (int b = a + 1; b < sum; b++)
+                {
+                    for (int c = b + 1; c < sum; c++)
+                    {
+                        if ((a + b + c) == sum && Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2))
+                        {
+                            return new int[] { a, b, c };
+                        }
+                    }
+                }
+            }
+
+            return new int[] { 0 };
+        }
+    }
+}
---
+++ Euler.Tests/PythagoreanTripletTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Extensions;
+using Euler.Lib;
+using Xunit;
+
+namespace Euler.Tests
+{
+    public class PythagoreanTripletTests
+    {
+        [Theory]
+        [InlineData(new int [] { 3, 4, 5 }, 12)]
+        [InlineData(new int[] { 200, 375, 425 }, 1000)] 
+        public void ShouldFindProductOfAAndB(int[] expected, int sum)
+        {
+            var sut = new PythagoreanTripletFinder();
+            var actual = sut.GetTriplets(sum);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
