d0f32935670bb670de04b23f61dd320ed52a1dff
Jaxwood <lorenzen.jacob@gmail.com>
2014-02-20 17:54:36 +0100

    solution to problem 13

---
+++ Euler.Lib/SumDigitProcessor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace Euler.Lib
+{
+    public class SumDigitProcessor
+    {
+        private int digitlength;
+
+        public SumDigitProcessor(int digitlength)
+        {
+            this.digitlength = digitlength;
+        }
+        public BigInteger Sum(string digitsequence)
+        {
+            var seq = this.ConvertToArray(digitsequence);
+            var result = BigInteger.Zero;
+            
+            for(int i = 0; i < seq.Length; i += digitlength)
+            {
+                result = BigInteger.Add(result, seq.Skip(i).Take(digitlength).Merge());
+            }
+
+            return result;
+        }
+
+        private int[] ConvertToArray(string series)
+        {
+            return series.Select(c => (int)Char.GetNumericValue(c)).ToArray();
+        }
+    }
+
+    internal static class MergeExtension
+    {
+        public static BigInteger Merge(this IEnumerable<int> digits)
+        {
+            var val = BigInteger.Zero;
+            return digits.Select(c => Convert.ToInt64(c))
+                        .Reverse()
+                        .Select((c, i) => BigInteger.Multiply(c, BigInteger.Pow(10, i)))
+                        .Aggregate(val, (acc, next) => BigInteger.Add(acc, next));
+        }
+    }
+}
---
+++ Euler.Tests/SumDigitTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Extensions;
+using Euler.Lib;
+using Xunit;
+
+namespace Euler.Tests
+{
+    public class SumDigitTests
+    {
+        [Theory]
+        [InlineData(2736, "123456789123456789", 3)]
+        [InlineData(495, "123456789123456789", 2)]
+        public void ShouldSumDigits(long expected, string digitsequence, int digitlength)
+        {
+            var sut = new SumDigitProcessor(digitlength);
+            var actual = sut.Sum(digitsequence);
+            
+            Assert.Equal(expected, actual);
+        }
+    }
+}
