b13fc53cfc2fc05bcf36772cadef3af1e6eaf71b
Jaxwood <lorenzen.jacob@gmail.com>
2014-02-23 16:30:26 +0100

    made test pass

--- Euler.Lib/MaximumPathFinder.cs
+++ Euler.Lib/MaximumPathFinder.cs
@@ -9,6 +9,26 @@ namespace Euler.Lib
     {
         public int GetMax(int[][] triangle)
         {
+            int index = 0;
+            int sum = 0;
+
+            foreach (var arr in triangle)
+            {
+                if (arr.Length == 1) 
+                {
+                    sum += arr[0];
+                }
+                else
+                {
+                    var tmpArr = arr.Skip(index).Take(2);
+                    var val = tmpArr.Max();
+                    sum += val;
+                    index = Array.IndexOf(arr, val);
+                }
+            }
+
+            return sum;
+
             return 0;
         }
     }
