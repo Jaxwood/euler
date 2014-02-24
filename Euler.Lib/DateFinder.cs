99abbf81617b08fdf1e2ed16ef1cdec60ac9e3f1
Jaxwood <lorenzen.jacob@gmail.com>
2014-02-24 18:16:36 +0100

    added tests to problem 19

---
+++ Euler.Lib/DateFinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Euler.Lib
+{
+    public class DateFinder
+    {
+        public int GetMonthsWithDay(DateTime from, DateTime to, DayOfWeek day)
+        {
+            int cnt = 0;
+
+            while (from != to)
+            {
+                if (from.DayOfWeek == day)
+                {
+                    cnt++;
+                }
+
+                from = from.AddMonths(1);
+            }
+
+            return cnt;
+        }
+    }
+}
---
+++ Euler.Tests/DateFinderTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Euler.Lib;
+using Xunit.Extensions;
+
+namespace Euler.Tests
+{
+    public class DateFinderTests
+    {
+        [Theory]
+        [InlineData(2, "1901-01-01", "1902-01-01", DayOfWeek.Sunday)]
+        [InlineData(2, "2013-01-01", "2014-01-01", DayOfWeek.Sunday)] 
+        public void ShouldReturnTheNumberOfSundaysOnDate(int expected, string from, string to, DayOfWeek day)
+        {
+            var sut = new DateFinder();
+            var dt = DateTime.Now;
+            
+            var actual = sut.GetMonthsWithDay(DateTime.Parse(from), DateTime.Parse(to), day);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
