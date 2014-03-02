1c98450848b12d0a9d0680676f12666008838201
Jaxwood <lorenzen.jacob@gmail.com>
2014-03-02 14:12:53 +0100

    solution to problem 29

---
+++ Euler.Lib/DistinctPower.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace Euler.Lib
+{
+    public class DistinctPower
+    {
+        public BigInteger[] GetPowers(int low, int high)
+        {
+            int count = (high - low) + 1;
+            return (from a in Enumerable.Range(low, count)
+                    from b in Enumerable.Range(low, count)
+                    select BigInteger.Pow(a, b))
+                    .Distinct()
+                    .OrderBy(c => c)                    
+                    .ToArray();
+        }
+
+        public int GetNumberOfTerms(int low, int high)
+        {
+            return this.GetPowers(low, high).Count();
+        }
+    }
+}
---
+++ Euler.Tests/DistinctPowerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+using Euler.Lib;
+using System.Numerics;
+
+namespace Euler.Tests
+{
+    public class DistinctPowerTests
+    {
+        [Fact]
+        public void ShouldFindAllDistinctPowersInRange()
+        {
+            var expected = new BigInteger[] { 4, 8, 9, 16, 25, 27, 32, 64, 81, 125, 243, 256, 625, 1024, 3125 };
+            var low = 2;
+            var high = 5;
+
+            var sut = new DistinctPower();
+            var actual = sut.GetPowers(low, high);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(15, 2, 5)]
+        public void ShouldFindNumberOfDistinctTerms(int expected, int low, int high)
+        {
+            var sut = new DistinctPower();
+            var actual = sut.GetNumberOfTerms(low, high);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
