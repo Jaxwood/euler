2b22b5eed6e35a2f04cb24c952ac4bdb0bb7385a
Jaxwood <lorenzen.jacob@gmail.com>
2014-03-03 19:41:45 +0100

    solution for problem 30

---
+++ Euler.Lib/DigitPowerFinder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Euler.Lib
+{
+    public class DigitPowerFinder
+    {
+        private MathUtil _mathUtil;
+
+        public DigitPowerFinder(MathUtil mathUtil)
+        {
+            this._mathUtil = mathUtil;
+        }
+
+        public int[] GetNumbersWithPower(int power)
+        {
+            var upper = this.GetUpperLimit(power);
+            return (from d in Enumerable.Range(2, upper)
+                    let sum = this._mathUtil.GetDigits(d).Select(e => (int)Math.Pow(e, power)).Sum()
+                    where d == sum
+                    select d).ToArray();
+        }
+
+        public int GetSum(int power)
+        {
+            return this.GetNumbersWithPower(power).Sum();
+        }
+
+        private int GetUpperLimit(int power)
+        {
+            bool found = false;
+            int cnt = 2;
+            var max = (int)Math.Pow(9, power);
+            
+            while(!found)
+            {
+                var number = max * cnt;
+                var digits = this._mathUtil.GetDigits(number);
+                if (number > max * digits.Length)
+                {
+                    found = true;                    
+                }
+                else
+                { 
+                    cnt = cnt + 1;
+                }                
+            }
+
+            return max * cnt;
+        }
+    }
+}
---
+++ Euler.Tests/DigitPowerFinderTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+using Euler.Lib;
+
+namespace Euler.Tests
+{
+    public class DigitPowerFinderTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1634, 8208, 9474 }, 4)]
+        public void ShouldFindAllNumberThatIsTheSumOfItsPowers(int[] expected, int power)
+        {
+            var sut = new DigitPowerFinder(new MathUtil());
+            var actual = sut.GetNumbersWithPower(power);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(19316, 4)]
+        public void ShouldFindTheSumOfAllNumberThatIsTheSumOfItsPowers(int expected, int power)
+        {
+            var sut = new DigitPowerFinder(new MathUtil());
+            var actual = sut.GetSum(power);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
